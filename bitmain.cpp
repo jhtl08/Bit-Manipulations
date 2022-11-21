9d6fd3761057d287ddd454fc562c87db6d196e6e
Ong03G <paolo.ong@obf.ateneo.edu>
2022-11-21 20:54:50 +0800

    implemented on, off, and toggle

--- bitmain.cpp
+++ bitmain.cpp
@@ -6,53 +6,63 @@
 #include "bitmanipulations.h"
 #include <sstream>
 
+using namespace std;
+
 int main()
 {
   //initialize unsigned integer
   unsigned int bit = 0;
-  std::bitset<32> x(bit);
-  std::string command;
-  unsigned int bit_number;
-  std::string input;
+  bitset<32> x(bit);
 
+  string command;
+  unsigned int bit_number;
+  string input;
+
+  cout << x << endl;
   while (true)
   {
-    std::cout << "> ";
-    getline(std::cin, input);
-    std::stringstream ss1(input);
+    unsigned int mask = 0x0F >> 3;
+    bitset<32> y(mask);
+    cout << "> ";
+    getline(cin, input);
+    stringstream ss1(input);
     if (ss1 >> command)
     {
       if (ss1 >> bit_number)
       {
+        y <<= bit_number;
         if (command=="on")
         {
-          std::cout << "Function on will turn " << bit_number << " on.\n";
+          cout << "Function on will turn " << bit_number << " on.\n";
+          x |= y;
         }
         else if (command=="off")
         {
-          std::cout << "Function off will turn " << bit_number << " off.\n";
+          cout << "Function off will turn " << bit_number << " off.\n";
+          x &= (~y);
         }
         else if (command=="toggle")
         {
-          std::cout << "Function toggle will toggle " << bit_number << ".\n";
+          cout << "Function toggle will toggle " << bit_number << ".\n";
+          x ^= y;
         }
         else
         {
-          std::cout << "Invalid command. Try again"<<std::endl;
+          cout << "Invalid command. Try again"<< endl;
           continue;
         }
       }
       else if (command=="exit")
       {
-        std::cout << "Terminating Bit Manipulations..." << std::endl;
+        cout << "Terminating Bit Manipulations..." << endl;
         return 0;
       }
       else
       {
-        std::cout << "Invalid command. Try again"<<std::endl;
+        cout << "Invalid command. Try again"<< endl;
         continue;
       }
     }
-  std::cout << x << std::endl;
+  cout << x << endl;
   }
 }
